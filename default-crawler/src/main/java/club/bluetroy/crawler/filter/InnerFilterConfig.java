21c4fb864841835385da7f8ee725b166493eff9c
heyixin <764118015@qq.com>
2019-01-27 14:18:45 +0800

    重构内置过滤器、拆封未多个内置过滤器

--- default-crawler/src/main/java/club/bluetroy/crawler/filter/InnerFilterConfig.java
+++ default-crawler/src/main/java/club/bluetroy/crawler/filter/InnerFilterConfig.java
@@ -12,32 +12,53 @@ import club.bluetroy.crawler.domain.FilterConfig;
  */
 public class InnerFilterConfig {
     public static FilterConfig getDefaultFilterConfig() {
-        FilterConfig currentShowFaceConfig = new FilterConfig();
-        currentShowFaceConfig.setAddTimeDistance("1天");
-        currentShowFaceConfig.setCollect(200);
-        currentShowFaceConfig.setTitle("露脸");
+        FilterConfig currentShowFaceConfig = getcurrentShowFaceConfig(200);
+        FilterConfig currentHotConfig = getCurrentHotConfig(500);
+        FilterConfig monthShowFace = getMonthShowFaceConfig(1500);
+        FilterConfig monthHot = getMonthHotConfig(2000);
+        FilterConfig collectOverFourThousand = getCollectedConfig(5000);
+        return or(currentShowFaceConfig, currentHotConfig, monthShowFace, monthHot, collectOverFourThousand);
+    }
 
-        FilterConfig currentHotConfig = new FilterConfig();
-        currentHotConfig.setAddTimeDistance("1天");
-        currentHotConfig.setCollect(500);
+    public static FilterConfig getcurrentShowFaceConfig(int collect) {
+        return getFilterConfig("露脸", "1天", collect);
+    }
 
-        FilterConfig monthShowFace = new FilterConfig();
-        monthShowFace.setAddTimeDistance("1月");
-        monthShowFace.setTitle("露脸");
-        monthShowFace.setCollect(1500);
+    public static FilterConfig getCurrentHotConfig(int collect) {
+        return getFilterConfig(null, "1天", collect);
+    }
 
-        FilterConfig monthHot = new FilterConfig();
-        monthHot.setAddTimeDistance("1月");
-        monthHot.setCollect(2000);
+    public static FilterConfig getMonthShowFaceConfig(int collect) {
+        return getFilterConfig("露脸", "1月", collect);
+    }
 
-        FilterConfig collectOverFourThousand = new FilterConfig();
-        collectOverFourThousand.setCollect(4000);
+    public static FilterConfig getMonthHotConfig(int collect) {
+        return getFilterConfig(null, "1月", collect);
+    }
 
-        currentShowFaceConfig.setOrFilterConfig(currentHotConfig);
-        currentHotConfig.setOrFilterConfig(monthShowFace);
-        monthShowFace.setOrFilterConfig(monthHot);
-        monthHot.setOrFilterConfig(collectOverFourThousand);
+    public static FilterConfig getCollectedConfig(int collect) {
+        return getFilterConfig(null, null, collect);
+    }
 
-        return currentShowFaceConfig;
+    private static FilterConfig or(FilterConfig... filterConfigs) {
+        if (filterConfigs.length == 1) {
+            return filterConfigs[0];
+        }
+        for (int i = 0; i < filterConfigs.length - 1; i++) {
+            filterConfigs[i].setOrFilterConfig(filterConfigs[i + 1]);
+        }
+        return filterConfigs[0];
+    }
+
+    private static FilterConfig getFilterConfig(String title, String addTimeDistance, int collect) {
+        FilterConfig filterConfig = new FilterConfig();
+        filterConfig.setTitle(title);
+        filterConfig.setAddTimeDistance(addTimeDistance);
+        filterConfig.setCollect(collect);
+        return filterConfig;
+    }
+
+    public static FilterConfig getShowFaceConfigConfig(int collect) {
+        return getFilterConfig("露脸", null, collect);
     }
 }
